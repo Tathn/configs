ab1dc35003c2f2c67f8b045bfcbd4ba51a8c159f
Tathn <siewniak.patryk@gmail.com>
2024-08-28 23:06:49 +0200

    nvim config lua rewrite, added some options eg hybrid line nums

---
+++ nvim/nvim_config/init.lua
@@ -0,0 +1,109 @@
+vim.o.compatible = false
+
+local vim = vim
+local Plug = vim.fn['plug#']
+
+--   - Neovim (Linux/macOS/Windows): stdpath('data') . '/plugged'
+-- You can specify a custom plugin directory by passing it as the argument
+--   - e.g. `call plug#begin('~/.vim/plugged')`
+--   - Avoid using standard Vim directory names like 'plugin'
+vim.call("plug#begin")
+
+Plug('Shadorain/shadotheme')
+Plug('junegunn/fzf.vim')
+
+-- Python
+Plug('numirias/semshi', { ['do'] = ':UpdateRemotePlugins' })
+
+-- Autocompletion
+Plug('neovim/nvim-lspconfig')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-cmdline')
+Plug('hrsh7th/nvim-cmp')
+
+-- For vsnip users.
+Plug('hrsh7th/cmp-vsnip')
+Plug('hrsh7th/vim-vsnip')
+-- /Autocompletion
+
+vim.call("plug#end")
+
+vim.cmd.colorscheme("shado")
+
+vim.keymap.set("n", ";", "<cmd>Files<cr>")
+vim.keymap.set("n", "\"", "<cmd>GFiles<cr>")
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.scrolloff = 10
+vim.o.wrap = false
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.showcmd = true
+vim.o.showmode = true
+vim.o.showmatch = true
+vim.o.hlsearch = true
+vim.o.history = 1000
+
+-- Autocompletion
+local cmp = require'cmp'
+
+cmp.setup({
+snippet = {
+  -- REQUIRED - you must specify a snippet engine
+  expand = function(args)
+    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+  end,
+},
+window = {
+  -- completion = cmp.config.window.bordered(),
+  -- documentation = cmp.config.window.bordered(),
+},
+mapping = cmp.mapping.preset.insert({
+  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+  ['<C-Space>'] = cmp.mapping.complete(),
+  ['<C-e>'] = cmp.mapping.abort(),
+  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+}),
+sources = cmp.config.sources({
+  { name = 'nvim_lsp' },
+  { name = 'vsnip' }, -- For vsnip users.
+}, {
+  { name = 'buffer' },
+})
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+mapping = cmp.mapping.preset.cmdline(),
+sources = {
+  { name = 'buffer' }
+}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+mapping = cmp.mapping.preset.cmdline(),
+sources = cmp.config.sources({
+  { name = 'path' }
+}, {
+  { name = 'cmdline' }
+}),
+matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['pyright'].setup {
+capabilities = capabilities
+}
+-- /Autocompletion
