5b454dc8d4ae28af5945231d0b9a12c90d84e06b
Tathn <siewniak.patryk@gmail.com>
2024-10-27 10:11:24 +0100

    gitsigns seems to spawn a lot of git instances on some machines

--- nvim/nvim_config/lua/plugins/gitsigns.lua
+++ nvim/nvim_config/lua/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
-  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+--  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
   opts = {
     signs = {
       add          = { text = '┃' },
